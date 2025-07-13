a38648600d229f8c19d2bdef8d0366c18b16c1cb
Rizqy256 <rizqyrmuh@gmail.com>
2025-07-13 20:05:19 +0700

    update 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // <-- tambahkan ini
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // <-- tambahkan ini
+      '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    // 🔥 Ini penting untuk mencegah error 404 saat refresh halaman seperti /cart
+    fs: {
+      allow: ['..']
+    },
+    historyApiFallback: true
   }
 })
